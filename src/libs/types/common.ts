faf76039cac6a18859a054c867174929796c86b8
ali <erali.jurakulov@mail.ru>
2026-04-26 23:20:06 +0900

    feat: develop member schema model

--- src/libs/types/common.ts
+++ src/libs/types/common.ts
@@ -1,3 +1,7 @@
 export interface T {
     [key: string]: any;
 }
+
+// 'export' boshqa bir nechta qiymatlarni qaytarishimiz mumkin 
+/* export function test() {};
+ import {T, test } from "../libs/types/common"; */
