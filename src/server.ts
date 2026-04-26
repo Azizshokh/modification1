d31d22854497885be2f9c0be784a63ce5ab91536
ali <erali.jurakulov@mail.ru>
2026-04-26 18:59:39 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -6,3 +6,12 @@ dotenv.config();
 console.log("Port:", process.env.PORT);
 console.log("Mongo_Url:", process.env.MONGO_URL);
 
+import mongoose from "mongoose";
+mongoose
+    .connect(process.env.MONGO_URL as string, {})
+    .then((data) => {
+        console.log("Connected to MongoDB");
+        const PORT = process.env.PORT || 3013;
+    })
+    .catch((err) => console.error("Error connecting to MongoDB:", err));
+
