d0448849b693588e09f91b9e739fdbec6fe61e85
ali <erali.jurakulov@mail.ru>
2026-04-28 15:52:22 +0900

    fix: organize SPA & SSR business logic

--- src/controllers/market.controller.ts
+++ src/controllers/market.controller.ts
@@ -4,6 +4,7 @@ import MemberService from "../models/Member.service";
 import { LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 
+const memberService = new MemberService();
 const marketController: T = {};
 marketController.goHome = (req: Request, res: Response) => {
     try {
@@ -33,20 +34,6 @@ marketController.getSignup = (req: Request, res: Response) => {
     }
 };
 
-marketController.proccesLogin = async (req: Request, res: Response) => {
-    try {
-        const input: LoginInput = req.body;
-
-        const memberService = new MemberService();
-        const result = await memberService.proccessLogin(input);
-
-        res.send(result);
-    } catch (error) {
-        console.error("Error in proccesLogin:", error);
-        res.send(error);
-    }
-};
-
 marketController.proccesSignup = async (req: Request, res: Response) => {
     try {
         console.log("proccesSignup");
@@ -55,13 +42,28 @@ marketController.proccesSignup = async (req: Request, res: Response) => {
         const newMember: MemberInput = req.body;
         newMember.memberType = MemberType.ADMIN;
 
-        const memberService = new MemberService();
         const result = await memberService.proccessSignup(newMember);
+        //TODO: SESSIONS Authenticate
 
         res.send(result);
     } catch (error) {
         console.error("Error in proccesSignup:", error);
     }
 };
+
+marketController.proccesLogin = async (req: Request, res: Response) => {
+    try {
+        const input: LoginInput = req.body;
+        const result = await memberService.proccessLogin(input);
+
+        //TODO: SESSIONS Authenticate
+
+        res.send(result);
+    } catch (error) {
+        console.error("Error in proccesLogin:", error);
+        res.send(error);
+    }
+};
+
 
 export default marketController;
--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,9 +1,42 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
+import { LoginInput, Member, MemberInput } from "../libs/types/member";
+import MemberService from "../models/Member.service";
+import Errors from "../libs/Error";
 
 // REACT
 
+const memberService = new MemberService();
 const memberController: T = {};
 
+memberController.signup = async (req: Request, res: Response) => {
+    try {
+        console.log("signup");
+        const input: MemberInput = req.body;
+        const result: Member = await memberService.signup(input);
+        // TODO: TOKEN
+
+        res.json({ member: result });
+    } catch (error) {
+        console.error("Error in signup:", error);
+        if (error instanceof Errors) res.status(error.code).json(error);
+        else res.status(Errors.standard.code).json(Errors.standard);
+    }
+};
+
+memberController.login = async (req: Request, res: Response) => {
+    try {
+        console.log("login");
+        const input: LoginInput = req.body;
+        const result = await memberService.login(input);
+        // TODO: TOKEN
+
+        res.json({ member: result });
+    } catch (error) {
+        console.error("Error in login:", error);
+        if (error instanceof Errors) res.status(error.code).json(error);
+        else res.status(Errors.standard.code).json(Errors.standard);
+    }
+};
 
 export default memberController;
