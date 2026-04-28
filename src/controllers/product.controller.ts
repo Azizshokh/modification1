689dc9d585a682b2b24c971b77ecfdcfd9b90f73
ali <erali.jurakulov@mail.ru>
2026-04-28 18:09:18 +0900

    feat: create product controller and product routing apis

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from "express";
+import Errors, { Message } from "../libs/Error";
+import { T } from "../libs/types/common";
+import ProductService from "../models/product.service";
+
+const productService = new ProductService();
+const productController: T = {};
+
+productController.getAllProducts = async (req: Request, res: Response) => {
+    try {
+        console.log("getAllProducts");
+        res.render("products");
+    } catch (error) {
+        console.error("Error in getAllProducts:", error);
+        if (error instanceof Errors) {
+            res.status(error.code).json(error);
+        } else {
+            res.status(500).json(error);
+        }
+    }
+}
+
+productController.createNewProduct = async (req: Request, res: Response) => {
+    try {
+        console.log("createNewProduct");
+    } catch (error) {
+        console.error("Error in createNewProduct:", error);
+        if (error instanceof Errors) {
+            res.status(error.code).json(error);
+        } else {
+            res.status(500).json(error);
+        }
+    }
+}
+
+productController.updateChosenProduct = async (req: Request, res: Response) => {
+    try {
+        console.log("updateChosenProduct");
+    } catch (error) {
+        console.error("Error in updateChosenProduct:", error);
+        if (error instanceof Errors) {
+            res.status(error.code).json(error);
+        } else {
+            res.status(500).json(error);
+        }
+    }
+}
+
+export default productController;
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const routerAdmin = express.Router();
 import marketController from "./controllers/market.controller";
+import productController from "./controllers/product.controller";
 
 /*** Admin Routes ***/
 routerAdmin.get("/", marketController.goHome);
@@ -14,6 +15,10 @@ routerAdmin.get("/logout", marketController.logout);
 routerAdmin.get("/check-me", marketController.checkAuthSession);
 
 /*** Product Routes ***/
+routerAdmin.get("/product/all", productController.getAllProducts);
+routerAdmin.post("/product/create", productController.createNewProduct);
+routerAdmin.post("/product/:id", productController.updateChosenProduct);
+
 /*** User Routes ***/
 
 export default routerAdmin;
