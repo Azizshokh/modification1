10feea9b684cf51cfa4ea2aa4a018e30a81d53b2
ali <erali.jurakulov@mail.ru>
2026-07-05 23:19:40 +0900

    feat: develop Socket.io server

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import { MORGAN_FORMAT } from "./libs/config";
+import { Server as SocketIOServer } from "socket.io";
+import http from "http";
 
 import session from "express-session";
 import ConnectMongo from "connect-mongodb-session";
@@ -64,4 +66,24 @@ app.set("view engine", "ejs");
 app.use("/admin", routerAdmin); // EJS =>  BSSR: Backend Server Side Rendering
 app.use("/", router);          // REACT => SPA: REACT Middleware Design Pattern
 
-export default app;
+const server = http.createServer(app);
+const io = new SocketIOServer(server, {
+    cors: {
+        origin: true,
+        credentials: true,
+    },
+});
+
+let summaryClient = 0;
+
+io.on("connection", (socket) => {
+    summaryClient++;
+    console.log(`Connection & total [${summaryClient}]`);
+
+    socket.on("disconnect", () => {
+        summaryClient--;
+        console.log(`Disconnect & total [${summaryClient}]`);
+    });
+});
+
+export default server;
